20fc22ebaca0d1b041fbe53dd09e009cab2f76cd
mynuddin62 <ratulmynuddin@yahoo.com>
2025-11-25 07:07:54 +0600

    [init commit]

---
+++ src/test/java/com/peacecoder/baseServer/BaseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peacecoder.baseServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
